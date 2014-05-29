64dbd76367fa846ad999dd3c08996a326b38c95b
Austin Ziegler <austin@clearfit.com>
2014-05-29 19:54:24 -0400

    Update to conversion API.
    - Mostly style changes (spaces instead of tabs), new_process instead of
      newProcess.
    - Changed how new_process works so that it really does create a new
      process when it's called.
      - This isn't ideal, but also suggests a larger API change than may
        have originally been planned (I'll put that under a separate PR to
        the pandurang90 repo).

--- lib/cloudconvert/conversion.rb
+++ lib/cloudconvert/conversion.rb
@@ -1,101 +1,105 @@
 module Cloudconvert
-	# cloudconvert.org API wrapper
-	class Conversion
-		def initialize(input_format , output_format)
-			@input_format = input_format
-			@output_format = output_format
-		end
+  class Conversion
+    ConversionFormatsNotDefined = Class.new(StandardError)
 
-		def newProcess
-			new_process_url = '/process'
-			payload = {
-				apikey: Cloudconvert.configuration.api_key,
-				inputformat: @input_format,
-				outputformat: @output_format
-			} if formats_defined?
-			callback_url = Cloudconvert.configuration.callback
-			payload[:callback] = callback_url unless callback_url.nil?
+    def initialize(input_format, output_format, options = {})
+      @input_format  = input_format
+      @output_format = output_format
+      @configuration = options.fetch(:configuration) {
+        options.fetch(:config) {
+          Cloudconvert.configuration
+        }
+      }
 
-			@proc_response ||= client.post(new_process_url, payload)
-			@processURL ||= @proc_response['url']
-		end
+      if input_format.empty? || output_format.empty?
+        raise ConversionFormatsNotDefined
+      end
+    end
 
-		def conversion_payload(options = {})
-			@conv_payload = {
-				input: "download",
-				outputformat: @output_format
-			}
-			@conv_payload.merge!(options)
-		end
+    def new_process
+      payload = {
+        apikey:       @configuration.api_key,
+        inputformat:  @input_format,
+        outputformat: @output_format
+      }
 
-		def request
-			client.post(@processURL, @conv_payload)
-		end
+      if @configuration.callback
+        payload[:callback] = @configuration.callback
+      end
 
-		def post_file(file, mime_type)
-			Faraday::UploadIO.new(file, mime_type)
-		end
+      @process_url = client.post('/process', payload)['url']
+    end
 
-		def upload_file
-			upload.post(@processURL, @conv_payload)
-		end
+    def conversion_payload(options = {})
+      @conv_payload = {
+        input: "download",
+        outputformat: @output_format
+      }
+      @conv_payload.merge!(options)
+    end
 
-		def status
-			client.get(@processURL)
-		end
+    def request
+      client.post(@process_url, @conv_payload)
+    end
 
-		def step
-			status['step']
-		end
+    def post_file(file, mime_type)
+      Faraday::UploadIO.new(file, mime_type)
+    end
 
-		def cancel
-			client.get("#{@processURL}/cancel")
-		end
+    def upload_file
+      upload.post(@process_url, @conv_payload)
+    end
 
-		def delete
-			client.get("#{@processURL}/delete")
-		end
+    def status
+      client.get(@process_url)
+    end
 
-		def list
-			client.get("/processes?apikey=#{Cloudconvert.configuration.api_key}")
-		end
+    def step
+      status['step']
+    end
 
-		def conversion_types(input_format = '', output_format = '')
-			uri = "/conversiontypes"
+    def cancel
+      client.get("#{@process_url}/cancel")
+    end
 
-			if (!input_format.empty? && !output_format.empty?)
-				uri += "?inputformat=#{input_format}&outputformat=#{output_format}"
-			elsif !input_format.empty?
-				uri += "?inputformat=#{input_format}"
-			elsif !output_format.empty?
-				uri += "?output_format=#{output_format}"
-			end
+    def delete
+      client.get("#{@process_url}/delete")
+    end
 
-			client.get(uri)
-		end
+    def list
+      client.get("/processes?apikey=#{Cloudconvert.configuration.api_key}")
+    end
 
-		def current_conversion_types
-			client.get("/conversiontypes?inputformat=#{@input_format}&outputformat=#{@output_format}")
-		end
+    def conversion_types(input_format = '', output_format = '')
+      uri = "/conversiontypes"
 
-		def download_link
-			current_status = status
-			current_status['output']['url'] if current_status['step'] == "finished"
-		end
+      if (!input_format.empty? && !output_format.empty?)
+        uri += "?inputformat=#{input_format}&outputformat=#{output_format}"
+      elsif !input_format.empty?
+        uri += "?inputformat=#{input_format}"
+      elsif !output_format.empty?
+        uri += "?output_format=#{output_format}"
+      end
 
-		private
+      client.get(uri)
+    end
 
-			def client
-				@client ||= Cloudconvert::Client.new
-			end
+    def current_conversion_types
+      client.get("/conversiontypes?inputformat=#{@input_format}&outputformat=#{@output_format}")
+    end
 
-			def upload
-				@upload ||= Cloudconvert::Upload.new
-			end
+    def download_link
+      current_status = status
+      current_status['output']['url'] if current_status['step'] == "finished"
+    end
 
-			def formats_defined?
-				raise "Conversion formats not defined!" if (@input_format.empty? || @output_format.empty?)
-				true
-			end
-	end
+    private
+    def client
+      @client ||= Cloudconvert::Client.new(@configuration)
+    end
+
+    def upload
+      @upload ||= Cloudconvert::Upload.new(@configuration)
+    end
+  end
 end
